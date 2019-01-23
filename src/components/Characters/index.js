acf630b4696c7e76ca8123816e50087c85eb786e
Kelvan Ince <incekelvan@gmail.com>
2019-01-23 17:06:00 -0500

    Add images add map method on characters props

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Avatar, Button, Grid, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 import { fetchCharacters } from '../../store/actions';
+import C3po from '../../images/c3po.png';
+import Luke from '../../images/luke.png';
+import Leia from '../../images/leia.png';
+import ObiWan from '../../images/obiwan.png';
+import Owen from '../../images/owen.png';
+import R2d2 from '../../images/r2d2.png';
+import R5d4 from '../../images/r5d4.png';
+import Vader from '../../images/vader.png';
+
+const styles = {
+  avatar: {
+    borderRadius: 0,
+    width: 330,
+    height: 450,
+  },
+}
 
 class Characters extends Component {
   constructor(props) {
@@ -18,10 +35,13 @@ class Characters extends Component {
     this.props.fetchCharacters();
   }
   render() {
-    const { characters } = this.props;
+    const { characters, classes } = this.props;
 
     return (
-      <Grid container justify="space-around" align="center">
+      <Grid container justify="space-around" align="center" spacing={40}>
+        <Grid item>
+          <Typography variant="primary">CHOOSE YOUR CHARACTER</Typography>
+        </Grid>
         {characters.map(ele => {
           return (
             <Grid item key={ele.name}>
@@ -29,6 +49,11 @@ class Characters extends Component {
             </Grid>
           )
         })}
+        <Grid container align="center" justify="space-around">
+          <Avatar className={classes.avatar} src={Luke} alt="Luke" />
+          <Avatar className={classes.avatar} src={C3po} alt="C3po" />
+          <Avatar className={classes.avatar} src={R2d2} alt="R2D2" />
+        </Grid>
         <Grid item>
           <Button onClick={this.handleClick}>Click</Button>
         </Grid>
@@ -49,4 +74,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Characters);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Characters));
